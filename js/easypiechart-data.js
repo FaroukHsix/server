84bbde276aa04448680d5689fc00546cb7c2fd48
Hammouda Farouk <hammouda_f@hotmail.fr>
2018-08-03 04:14:38 +0100

    server

---
+++ js/easypiechart-data.js
@@ -0,0 +1,48 @@
+$(function() {
+  $("#gate1").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate2").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate3").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate4").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate5").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate6").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
+
+$(function() {
+  $("#gate7").easyPieChart({
+    scaleColor: false,
+    barColor: "#f9243f"
+  });
+});
